d9d06f4b93af33517c86489b9b3896691d53ce66
Michael Bruce <me@michaelbruce.co>
2019-06-15 00:25:16 +0100

    boot operation builds an iso

--- boot/boot.go
+++ boot/boot.go
@@ -1,11 +1,14 @@
 package boot
 
 import (
+	"bufio"
 	"bytes"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"os"
 	"os/exec"
+	"os/user"
 	"sysconf/pacman"
 )
 
@@ -27,7 +30,55 @@ func Setup(packages pacman.Packages) {
 		os.Exit(1)
 	}
 
-	// defer os.RemoveAll(dir)
+	usr, err := user.Current()
+
+	if err != nil {
+		fmt.Println("could not find current user: ", err)
+		os.Exit(1)
+	}
+
+	_, err = exec.Command("cp", "-r", usr.HomeDir+"/.vim", dir+"/airootfs/root/.vim").Output()
+
+	if err != nil {
+		fmt.Println("could not copy .vim: ", err)
+		os.Exit(1)
+	}
+
+	if err = os.Mkdir(dir+"/airootfs/root/.config", 0755); err != nil {
+		fmt.Println("could not copy .vim: ", err)
+		os.Exit(1)
+	}
+
+	_, err = exec.Command("cp",
+		"-r",
+		usr.HomeDir+"/.config/openbox",
+		dir+"/airootfs/root/.config/openbox").Output()
+
+	if err != nil {
+		fmt.Println("could not copy .config/openbox: ", err)
+		os.Exit(1)
+	}
+
+	err = copyFiles([]copyInstruction{
+		copyInstruction{".bashrc", ".bashrc"},
+		copyInstruction{".xinitrc", ".xinitrc"},
+		copyInstruction{".vimrc", ".vimrc"},
+		copyInstruction{".Xresources", ".Xresources"},
+		copyInstruction{".Xmodmap", ".Xmodmap"},
+		copyInstruction{".xinitrc", ".xinitrc"},
+		copyInstruction{".tmux.conf", ".tmux.conf"},
+		copyInstruction{"src/sysconf/sysconf", "sysconf"},
+		copyInstruction{"src/sysconf/packages.json", "packages.json"},
+	}, dir)
+
+	if err != nil {
+		fmt.Println("problem copying files: %v", err)
+	}
+
+	appendSetBashInstructions(dir)
+	build(dir)
+
+	defer os.RemoveAll(dir)
 }
 
 // copies across the release engineering config
@@ -57,6 +108,54 @@ func config() string {
 	return dir
 }
 
+type copyInstruction struct {
+	Source      string
+	Destination string
+}
+
+// copy a file from src to dst
+func copy(src, dst string) error {
+	srcStat, err := os.Stat(src)
+	if err != nil {
+		return err
+	}
+
+	if !srcStat.Mode().IsRegular() {
+		return fmt.Errorf("%s is not a regular file", src)
+	}
+
+	srcFile, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer srcFile.Close()
+
+	dstFile, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+	defer dstFile.Close()
+
+	_, err = io.Copy(dstFile, srcFile)
+	return err
+}
+
+func copyFiles(instructions []copyInstruction, dir string) error {
+	usr, err := user.Current()
+	if err != nil {
+		return err
+	}
+
+	for _, inst := range instructions {
+		err := copy(usr.HomeDir+"/"+inst.Source, dir+"/airootfs/root/"+inst.Destination)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
 // The packages.x86_64 file in the archiso defines which packages are installed
 // on the live system. This function appends a few helpful extras.
 func appendBootPackages(dir string, packages pacman.Packages) error {
@@ -75,4 +174,36 @@ func appendBootPackages(dir string, packages pacman.Packages) error {
 	}
 
 	return nil
+}
+
+func appendSetBashInstructions(dir string) error {
+	f, err := os.OpenFile(dir+"/airootfs/root/customize_airootfs.sh", os.O_APPEND|os.O_WRONLY, 0600)
+	if err != nil {
+		return err
+	}
+
+	defer f.Close()
+
+	_, err = f.WriteString("usermod -s /usr/bin/bash root\n")
+	return err
+}
+
+func build(dir string) {
+	cmd := exec.Command("sudo", dir+"/build.sh", "-v", "-o", ".")
+	stdout, _ := cmd.StdoutPipe()
+	cmd.Start()
+
+	scanner := bufio.NewScanner(stdout)
+	scanner.Split(bufio.ScanLines)
+	for scanner.Scan() {
+		m := scanner.Text()
+		fmt.Println(m)
+	}
+	cmd.Wait()
+
+	_, err := exec.Command("sudo", "rm", "-rf", "./work").Output()
+	if err != nil {
+		fmt.Println("could not remove work folder: ", err)
+		os.Exit(1)
+	}
 }
